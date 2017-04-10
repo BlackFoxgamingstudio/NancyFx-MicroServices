4eb064a9498396eb9194cac30490867cdbd53b98
naruto <blackfoxgamingstudio@gmail.com>
2017-04-09 17:04:21 -0700

    Create Example response from the Shopping Cart microservice.cs

---
+++ shopingcart/Example response from the Shopping Cart microservice.cs
@@ -0,0 +1,28 @@
+Http/1.1 200 OK
+// the response body is in json
+Content-Type: application/json: charset-utf-8 
+// length of the response body
+539
+//shopping cart serialized as json
+{
+"userId": 42,
+"items": {
+	{
+		"productcatalogID": 1,
+			"productName": "Basic t-shirt",
+			"price":{
+					"currency": "eur",
+						"amount": 40
+							}
+							},
+							{
+								"productcatalogId": 2,
+									"productName":Fancy shirt",
+										"description": "a loud t-shirt",
+										"price": {
+												"currency": "eur",
+												"amount": 50
+												}
+												}
+												]
+												}
